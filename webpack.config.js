57984184143c2baff5af36fbd58fe688b0348fd2
Tory Shoreman <tory.shoreman@beamworks.io>
2019-07-16 16:55:20 -0400

    Move webpack config and dev server into same file

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './src/index.jsx'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/build/'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, 'src')
-      },
-      {
-        test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
-      }
-    ]
-  }
-  }
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            { test: /\.(jsx?)$/, use: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+            { test: /\.(jpg|jpeg|gif|png|svg)$/, use: 'file-loader', exclude: /node_modules/ }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({}),
 
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './index.html'
+        })
+    ],
+
+    devServer: {
+        inline: true,
+        historyApiFallback: true,
+        disableHostCheck: true
+    }
+};
