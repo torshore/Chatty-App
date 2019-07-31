512d5ca66337644c0852e7647728bfd9863c0822
Tory Shoreman <tory.shoreman@beamworks.io>
2019-07-30 22:40:42 -0400

    Refactor App and ChatBar to use hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,74 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-             currentUser: "Anonymous",
-             messages: [],
-        }
-    }
+export default function App(){
+    const [ currentUser, setCurrentUser ] = useState('Anonymous');
+    const [ messages, setMessages ] = useState([]);
+    const [ clientCount, setClientCount ] = useState(0);
 
-    addMessage = event => {
-        if (event.key === "Enter") {
-
+    const addMessage = (event) => {
+        if (event.key === 'Enter') {
             let newMessage = {
-              type: "postMessage",
-              username: this.state.currentUser,
+              type: 'postMessage',
+              username: currentUser,
               content: event.target.value
             };
+
             this.socket.send(JSON.stringify(newMessage));
-        };
-    }
-
-  changeUsername = (event) => {
-    if (event.key === "Enter") {
-      const oldName = this.state.currentUser;
-      const newName = event.target.value;
-      this.setState({currentUser: newName});
-      const newMessage = {
-        type: 'postNotification',
-        content: `${oldName} has changed their name to ${newName}`
-      }
-    console.log(newMessage);
-    this.socket.send(JSON.stringify(newMessage));
-  }
-  }
-
-  componentDidMount() {
-    this.socket = new WebSocket("ws://localhost:3001/");
-
-    this.socket.onopen = (event) => {
-      console.log("28, Connected to server!")
-    };
-
-    this.socket.onmessage = (event) => {
-      let data = JSON.parse(event.data);
-      console.log(52, data);
-      console.log("Message from socket server: ", data)
-        switch(data.type) {
-          case "incomingNotification":
-          case "incomingMessage":
-            let oldMsgs = this.state.messages;
-            let msgList = oldMsgs.concat(data);
-            this.setState({messages: msgList});
-            break;
-            case "incomingclientCount":
-            this.setState({clientCount: data.quantity})
-            console.log(data.quantity)
-            break;
-          default:
-            console.error("unknown message type")
         }
-      }
     }
 
-  render() {
-    return (
-      <div className='wrapper'>
+    const changeUsername = (event) => {
+        if (event.key === 'Enter') {
+            const oldName = currentUser;
+            const newName = event.target.value;
+            const newMessage = {
+                type: 'postNotification',
+                content: `${oldName} has changed their name to ${newName}`
+            }
+
+            setCurrentUser(newName);
+            this.socket.send(JSON.stringify(newMessage));
+        }
+    }
+
+    useEffect(() => {
+        const socket = new WebSocket('ws://localhost:3001/');
+
+        socket.onopen = () => {
+            console.log('28, Connected to server!')
+        };
+
+        socket.onmessage = (event) => {
+            let data = JSON.parse(event.data);
+
+            switch(data.type) {
+            case 'incomingNotification':
+                break;
+            case 'incomingMessage':
+                setMessages(messages.concat(data));
+                break;
+            case 'incomingclientCount':
+                setClientCount(data.quantity)
+                break;
+            default:
+                console.error('unknown message type')
+            }
+        }
+    })
+
+    return <div className='wrapper'>
         <nav className="navbar">
           <h1 className="navbar-brand">Chatty</h1>
-          <div className="client-count"> {this.state.clientCount} users online </div>
+          <div className="client-count"> {clientCount} users online </div>
         </nav>
-        <MessageList message={this.state.messages}/>
+
+        <MessageList message={messages}/>
+
         <ChatBar
-          changeUsername={this.changeUsername}
-          addMessage={this.addMessage}
+          changeUsername={(event) => changeUsername(event)}
+          addMessage={(event) => addMessage(event)}
         />
-      </div>
-    );
-  }
+    </div>
 }
